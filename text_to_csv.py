2fc00170e18c07bf6d3ddfd56c031c0646535a44
Keith Goh <keithgoh999@gmail.com>
2025-03-02 03:00:39 +0800

    convert to csv

--- text_to_csv.py
+++ text_to_csv.py
@@ -1,19 +1,21 @@
 import csv
 
-input_file = "data/c2.txt"
-output_file = "data/CNN_2.csv"
+lst = ["bbc", "brookings", "cna", "cnn", "forbes", "guardian", "straitstimes"]
+for e in lst:
+    input_file = f"data/living/{e}2.txt"
+    output_file = f"data/{e}2.csv"
 
 # Open the txt file
-file = open(input_file, 'r', encoding = "utf-8")
-new_text = file.readlines()
+    file = open(input_file, 'r', encoding = "utf-8")
+    new_text = file.readlines()
 
-text = [s for s in new_text if s != "\n"]
-text2 = [s[:-1] for s in text]
+    text = [s for s in new_text if s != "\n"]
+    text2 = [s[:-1] for s in text]
 
-# Open the csv file in write mode
-with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
-    writer = csv.writer(csv_file)
+    # Open the csv file in write mode
+    with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
 
-    # Write each sentence as a new row in the CSV
-    for line in text2:
-        writer.writerow([line])  # Write as a single column in CSV
+        # Write each sentence as a new row in the CSV
+        for line in text2:
+            writer.writerow([line])  # Write as a single column in CSV
