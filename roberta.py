b63438e862552a99bd5cb95f0b394b0d36707b39
Keith Goh <keithgoh999@gmail.com>
2025-03-01 19:06:33 +0800

    csv and sentiment scoring

--- roberta.py
+++ roberta.py
@@ -1,8 +1,8 @@
 # Activate virtual environment first through venv/Scripts/activate
 # and install dependencies in requirements.txt
-import torch
 import pandas as pd
 import numpy as np
+import tqdm
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
 
 class SimpleDataset:
@@ -14,25 +14,39 @@ class SimpleDataset:
 
     def __getitem__(self, idx):
         return {k: v[idx] for k, v in self.tokenized_texts.items()}
+
+def sentimentAnalysis(news_dict: dict[str, list[str]]):
+    model_name = "siebert/sentiment-roberta-large-english"
+    tokeniser = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    trainer = Trainer(model=model)
+
+    res = {}
+    for company, articleList in tqdm.tqdm(news_dict.items()):
+        scoreList = []
+        for article in articleList:
+            scoreList.append(get_score(article, tokeniser, trainer))
+        
+        scoreArray = np.array(scoreList)
+        mean = scoreArray.mean()
+
+        res[company] = float(mean)
+    return res
+
+        
+def get_score(file: str, tokeniser, trainer):
+    filename = 'data/' + file
+    data = pd.read_csv(filename)
     
-model_name = "siebert/sentiment-roberta-large-english"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-trainer = Trainer(model=model)
+    raw_sentences = data["text"]
+    sentences = [s for s in raw_sentences if len(s) > 1]
 
-data = pd.read_csv("bbc-text-1.csv")
-tech = data[data["category"] == "tech"]
-df1 = pd.DataFrame(tech)
-pred_texts = list(df1["text"][0:5])
+    tokenized_texts = tokeniser(sentences,truncation=True,padding=True)
+    dataset = SimpleDataset(tokenized_texts)
+    predictions = trainer.predict(dataset)
 
-tokenized_texts = tokenizer(pred_texts,truncation=True,padding=True)
-pred_dataset = SimpleDataset(tokenized_texts)
-predictions = trainer.predict(pred_dataset)
+    scores = predictions.predictions.argmax(-1)
+    return scores.mean()
 
-preds = predictions.predictions.argmax(-1)
-labels = pd.Series(preds).map(model.config.id2label)
-scores = (np.exp(predictions[0])/np.exp(predictions[0]).sum(-1,keepdims=True)).max(1)
-
-df = pd.DataFrame(list(zip(pred_texts,preds,labels,scores)), columns=['text','pred','label','score'])
-
-print(df.head())
+score = sentimentAnalysis({"Brookings": ["Brookings_1.csv", "Brookings_2.csv"], "HBR": ["HBR_1.csv"]})
+print(score)
